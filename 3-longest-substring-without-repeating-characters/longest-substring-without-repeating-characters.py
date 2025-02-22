d62aba934113a0e4cddf1de5531cc7f7cfff4cd6
Fernando Prange Menechini <134608398+FPrangeM@users.noreply.github.com>
2025-02-22 12:47:18 -0300

    Time: 11 ms (95.41%) | Memory: 18 MB (12.46%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,14 +1,29 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
 
-        N=0
-        for i in range(len(s)):
-            n=0
-            for f in range(i+1,len(s)+1):
-                subs = s[i:f]
-                if subs.count(subs[-1]) == 2:
-                    break
-                N = max(N,len(subs))
+        # N=0
+        # for i in range(len(s)):
+        #     n=0
+        #     for f in range(i+1,len(s)+1):
+        #         subs = s[i:f]
+        #         if subs.count(subs[-1]) == 2:
+        #             break
+        #         N = max(N,len(subs))
 
 
-        return N
+        # return N
+
+        char_index = {}  # Dicionário para armazenar o índice do último caractere visto
+        start = 0       # Início da janela deslizante
+        max_length = 0  # Comprimento máximo da substring encontrada
+
+        for end, char in enumerate(s):
+            if char in char_index and char_index[char] >= start:
+                # Se o caractere já foi visto e está dentro da janela atual,
+                # move o início da janela para a próxima posição após a repetição.
+                start = char_index[char] + 1
+            
+            char_index[char] = end  # Atualiza o índice do caractere atual
+            max_length = max(max_length, end - start + 1)  # Calcula e atualiza o comprimento máximo
+
+        return max_length
