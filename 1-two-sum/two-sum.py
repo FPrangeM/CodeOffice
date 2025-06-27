b48be2c02afba17ad2687f08bb49c3fcd1820b08
Fernando Prange Menechini <134608398+FPrangeM@users.noreply.github.com>
2025-06-26 23:03:21 -0300

    Time: 2677 ms (10.79%) | Memory: 13.4 MB (15.64%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,20 +1,15 @@
-class Solution:
-    # O(n^2)
-    def twoSum2(self, nums: List[int], target: int) -> List[int]:
+class Solution(object):
+    def twoSum(self, nums, target):
+        # nums = [lambda x: x if x <= target else 0.1 for x in nums]
 
-        for i1 in range(len(nums)):
-            for i2 in range(i1+1,len(nums)):
-                if nums[i1]+nums[i2] == target:
+        n = len(nums)
+        s = 0
+
+        for i1 in range(n):
+            for i2 in range(i1+1,n):
+                n1=nums[i1]
+                n2=nums[i2]
+                s = n1+n2
+                if s == target:
                     return [i1,i2]
-        return None
 
-    # O(n)
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-
-        seem={}
-        for i,num in enumerate(nums):
-            complement = target - num
-            if complement in seem:
-                return [i,seem[complement]]
-            else:
-                seem[num] = i
