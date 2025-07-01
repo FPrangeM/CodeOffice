2a6d88053cd5b6834415f4da71335076c0de7bb2
Fernando Prange Menechini <134608398+FPrangeM@users.noreply.github.com>
2025-06-30 22:52:14 -0300

    Time: 367 ms (15.57%) | Memory: 13.1 MB (63.33%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,29 +1,24 @@
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-
-        # N=0
-        # for i in range(len(s)):
-        #     n=0
-        #     for f in range(i+1,len(s)+1):
-        #         subs = s[i:f]
-        #         if subs.count(subs[-1]) == 2:
-        #             break
-        #         N = max(N,len(subs))
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        max_length = 0
+        substring = ''
 
 
-        # return N
+        for window_start in range(len(s)):
+            window_end = window_start+1
 
-        char_index = {}  # Dicionário para armazenar o índice do último caractere visto
-        start = 0       # Início da janela deslizante
-        max_length = 0  # Comprimento máximo da substring encontrada
+            while window_end != len(s) and s[window_end] not in s[window_start:window_end]:
+                window_end +=1
 
-        for end, char in enumerate(s):
-            if char in char_index and char_index[char] >= start:
-                # Se o caractere já foi visto e está dentro da janela atual,
-                # move o início da janela para a próxima posição após a repetição.
-                start = char_index[char] + 1
+            if len(s[window_start:window_end]) > max_length:
+                max_length = len(s[window_start:window_end])
             
-            char_index[char] = end  # Atualiza o índice do caractere atual
-            max_length = max(max_length, end - start + 1)  # Calcula e atualiza o comprimento máximo
+            if window_end == len(s):
+                break
+
 
         return max_length
